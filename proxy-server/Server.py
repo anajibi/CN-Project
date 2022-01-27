bd38a3b9ebe709485e4f1c8819ca00df744f02dd
Ali Najibi <njibiali@gmail.com>
2022-01-27 14:00:53 +0330

    Proxy server structure

--- proxy-server/Server.py
+++ proxy-server/Server.py
@@ -1,4 +1,8 @@
 # Proxy Input Request Port
+import socket
+from enum import Enum
+from typing import Dict, Tuple
+
 CHAT_PORT = 6060
 STREAMING_PORT = 6070
 
@@ -9,5 +13,13 @@ SERVER_PORT_PUBLISH = 3030
 SERVER_PORT_INFO = 4030
 
 
+class SockType(Enum):
+    TCP = "TCP",
+    UDP = "UDP"
+
+
 class ProxyServer:
-    pass
+    chat: socket.socket
+    media_tcp: socket.socket
+    media_udp: socket.socket
+    forwarding_table: Dict[Tuple[Tuple[str, int], SockType], int]
