13c474b3411ca691625c760cfe2e69e9a99a6eb7
Ali Sharifi <a.sharifi1379@gmail.com>
2022-02-06 20:35:07 +0330

    graceful termination

--- streaming-server/testclient.py
+++ streaming-server/testclient.py
@@ -8,6 +8,7 @@ BUFF_SIZE = 65536
 BREAK = False
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+client_socket.settimeout(1)
 host_name = socket.gethostname()
 host_ip = '127.0.0.1'
 print(host_ip)
@@ -22,29 +23,31 @@ def video_stream():
 	cv2.moveWindow('RECEIVING VIDEO', 10,360) 
 	fps,st,frames_to_count,cnt = (0,0,20,0)
 	while True:
-		packet,_ = client_socket.recvfrom(BUFF_SIZE)
-		data = base64.b64decode(packet,' /')
-		npdata = np.fromstring(data,dtype=np.uint8)
-	
-		frame = cv2.imdecode(npdata,1)
-		frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-		cv2.imshow("RECEIVING VIDEO",frame)
-		key = cv2.waitKey(1) & 0xFF
-	
-		if key == ord('q'):
-			client_socket.close()
+		try:
+			packet,_ = client_socket.recvfrom(BUFF_SIZE)
+			data = base64.b64decode(packet,' /')
+			npdata = np.fromstring(data,dtype=np.uint8)
+		
+			frame = cv2.imdecode(npdata,1)
+			frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+			cv2.imshow("RECEIVING VIDEO",frame)
+			key = cv2.waitKey(1) & 0xFF
+		
+			if key == ord('q'):
+				client_socket.close()
+				os._exit(1)
+				break
+
+			if cnt == frames_to_count:
+				try:
+					fps = round(frames_to_count/(time.time()-st))
+					st=time.time()
+					cnt=0
+				except:
+					pass
+			cnt+=1
+		except:
 			os._exit(1)
-			break
-
-		if cnt == frames_to_count:
-			try:
-				fps = round(frames_to_count/(time.time()-st))
-				st=time.time()
-				cnt=0
-			except:
-				pass
-		cnt+=1
-
 
 def audio_stream():
 	
@@ -81,7 +84,6 @@ def audio_stream():
 			stream.write(frame)
 
 		except:
-			
 			break
 
 	client_socket.close()
