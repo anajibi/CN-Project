48e2e23a135c5e2c002ae785789981eb289fea22
Alireza <honarvar45@gmail.com>
2022-02-06 21:42:13 +0330

    Make Streaming Menu

--- client/menu/StreamingMenu.py
+++ client/menu/StreamingMenu.py
@@ -1,14 +1,16 @@
+import base64
+import os
 import pickle
 import socket
 import struct
+from datetime import time
 
 import cv2
+import numpy as np
+import pyaudio
 
 from client.Firewall import ControlledSocket
 from client.menu.Menu import Menu
-
-IP, port = '127.0.0.1', 4030
-
 
 class StreamingMenu(Menu):
     publish_socket: ControlledSocket
@@ -21,36 +23,94 @@ class StreamingMenu(Menu):
         self.stream_socket = ControlledSocket(socket.AF_INET, socket.SOCK_STREAM)
 
     def show(self):
-        self.publish_socket.connect((IP, port))
-        msg = self.publish_socket.recv(1024)
-        print(msg.decode('ascii'))
+        BUFF_SIZE = 65536
 
-        payload_size = struct.calcsize('Q')
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
+        client_socket.settimeout(1)
+        host_ip = 'localhost'
+        print(host_ip)
+        message = b'mahmooti'
+        client_socket.sendto(message, (host_ip, self.streaming_port))
 
-        self.stream_socket.connect((IP, port + 1))
-        self.stream_socket.sendall(b'behdad babaei')
-        data = b''
-        while True:
-            while len(data) < payload_size:
-                packet = self.stream_socket.recv(4096)
-                if not packet:
+        def video_stream():
+            cv2.namedWindow('RECEIVING VIDEO')
+            cv2.moveWindow('RECEIVING VIDEO', 10, 360)
+            fps, st, frames_to_count, cnt = (0, 0, 20, 0)
+            while True:
+                try:
+                    packet, _ = client_socket.recvfrom(BUFF_SIZE)
+                    data = base64.b64decode(packet, b' /')
+                    npdata = np.fromstring(data.decode(), dtype=np.uint8)
+
+                    frame = cv2.imdecode(npdata, 1)
+                    frame = cv2.putText(frame, 'FPS: ' + str(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255),
+                                        2)
+                    cv2.imshow("RECEIVING VIDEO", frame)
+                    key = cv2.waitKey(1) & 0xFF
+
+                    if key == ord('q'):
+                        client_socket.close()
+                        os._exit(1)
+                        break
+
+                    if cnt == frames_to_count:
+                        try:
+                            fps = round(frames_to_count / (time.time() - st))
+                            st = time.time()
+                            cnt = 0
+                        except:
+                            pass
+                    cnt += 1
+                except:
+                    exit(1)
+
+        def audio_stream():
+            p = pyaudio.PyAudio()
+            CHUNK = 1024
+            stream = p.open(format=p.get_format_from_width(2),
+                            channels=2,
+                            rate=44100,
+                            output=True,
+                            frames_per_buffer=CHUNK)
+
+            # create socket
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket_address = (host_ip, self.streaming_port)
+            print('server listening at', socket_address)
+            client_socket.connect(socket_address)
+            print("CLIENT CONNECTED TO", socket_address)
+            data = b""
+            payload_size = struct.calcsize("Q")
+            while True:
+                try:
+                    while len(data) < payload_size:
+                        packet = client_socket.recv(4 * 1024)  # 4K
+                        if not packet:
+                            break
+                        data += packet
+                    packed_msg_size = data[:payload_size]
+                    data = data[payload_size:]
+                    msg_size = struct.unpack("Q", packed_msg_size)[0]
+                    while len(data) < msg_size:
+                        data += client_socket.recv(4 * 1024)
+                    frame_data = data[:msg_size]
+                    data = data[msg_size:]
+                    frame = pickle.loads(frame_data)
+                    stream.write(frame)
+
+                except:
                     break
-                data += packet
-            frame_size = struct.unpack('Q', data[:payload_size])[0]
-            data = data[payload_size:]
-            while len(data) < frame_size:
-                packet = self.stream_socket.recv(4096)
-                data += packet
-            serialized_frame = data[:frame_size]
-            data = data[frame_size:]
-            frame = pickle.loads(serialized_frame)
-            cv2.imshow('behdad babaei', frame)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
 
-        self.publish_socket.close()
-        self.stream_socket.close()
-        cv2.destroyAllWindows()
+            client_socket.close()
+            print('Audio closed')
+            exit(1)
+
+        from concurrent.futures import ThreadPoolExecutor
+
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            executor.submit(audio_stream)
+            executor.submit(video_stream)
 
         return self
 
--- streaming-server/testclient.py
+++ streaming-server/testclient.py
@@ -1,98 +1,105 @@
-import cv2, imutils, socket
-import numpy as np
-import time, os
 import base64
-import threading, wave, pyaudio,pickle,struct
+import os
+import pickle
+import pyaudio
+import struct
+import threading
+import time
+import wave
+
+import cv2
+import imutils
+import numpy as np
+import socket
+
 BUFF_SIZE = 65536
 
-BREAK = False
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
 client_socket.settimeout(1)
 host_name = socket.gethostname()
 host_ip = '127.0.0.1'
 print(host_ip)
 port = 4031
 message = b'mahmooti'
-client_socket.sendto(message,(host_ip,port))
+client_socket.sendto(message, (host_ip, port))
 
 
 def video_stream():
-	
-	cv2.namedWindow('RECEIVING VIDEO')        
-	cv2.moveWindow('RECEIVING VIDEO', 10,360) 
-	fps,st,frames_to_count,cnt = (0,0,20,0)
-	while True:
-		try:
-			packet,_ = client_socket.recvfrom(BUFF_SIZE)
-			data = base64.b64decode(packet,' /')
-			npdata = np.fromstring(data,dtype=np.uint8)
-		
-			frame = cv2.imdecode(npdata,1)
-			frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-			cv2.imshow("RECEIVING VIDEO",frame)
-			key = cv2.waitKey(1) & 0xFF
-		
-			if key == ord('q'):
-				client_socket.close()
-				os._exit(1)
-				break
+    cv2.namedWindow('RECEIVING VIDEO')
+    cv2.moveWindow('RECEIVING VIDEO', 10, 360)
+    fps, st, frames_to_count, cnt = (0, 0, 20, 0)
+    while True:
+        try:
+            packet, _ = client_socket.recvfrom(BUFF_SIZE)
+            data = base64.b64decode(packet, b' /')
+            npdata = np.fromstring(data.decode(), dtype=np.uint8)
 
-			if cnt == frames_to_count:
-				try:
-					fps = round(frames_to_count/(time.time()-st))
-					st=time.time()
-					cnt=0
-				except:
-					pass
-			cnt+=1
-		except:
-			os._exit(1)
+            frame = cv2.imdecode(npdata, 1)
+            frame = cv2.putText(frame, 'FPS: ' + str(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.imshow("RECEIVING VIDEO", frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == ord('q'):
+                client_socket.close()
+                os._exit(1)
+                break
+
+            if cnt == frames_to_count:
+                try:
+                    fps = round(frames_to_count / (time.time() - st))
+                    st = time.time()
+                    cnt = 0
+                except:
+                    pass
+            cnt += 1
+        except:
+            os._exit(1)
+
 
 def audio_stream():
-	
-	p = pyaudio.PyAudio()
-	CHUNK = 1024
-	stream = p.open(format=p.get_format_from_width(2),
-					channels=2,
-					rate=44100,
-					output=True,
-					frames_per_buffer=CHUNK)
-					
-	# create socket
-	client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	socket_address = (host_ip,port)
-	print('server listening at',socket_address)
-	client_socket.connect(socket_address) 
-	print("CLIENT CONNECTED TO",socket_address)
-	data = b""
-	payload_size = struct.calcsize("Q")
-	while True:
-		try:
-			while len(data) < payload_size:
-				packet = client_socket.recv(4*1024) # 4K
-				if not packet: break
-				data+=packet
-			packed_msg_size = data[:payload_size]
-			data = data[payload_size:]
-			msg_size = struct.unpack("Q",packed_msg_size)[0]
-			while len(data) < msg_size:
-				data += client_socket.recv(4*1024)
-			frame_data = data[:msg_size]
-			data  = data[msg_size:]
-			frame = pickle.loads(frame_data)
-			stream.write(frame)
+    p = pyaudio.PyAudio()
+    CHUNK = 1024
+    stream = p.open(format=p.get_format_from_width(2),
+                    channels=2,
+                    rate=44100,
+                    output=True,
+                    frames_per_buffer=CHUNK)
 
-		except:
-			break
+    # create socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_address = (host_ip, port)
+    print('server listening at', socket_address)
+    client_socket.connect(socket_address)
+    print("CLIENT CONNECTED TO", socket_address)
+    data = b""
+    payload_size = struct.calcsize("Q")
+    while True:
+        try:
+            while len(data) < payload_size:
+                packet = client_socket.recv(4 * 1024)  # 4K
+                if not packet: break
+                data += packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += client_socket.recv(4 * 1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)
+            stream.write(frame)
 
-	client_socket.close()
-	print('Audio closed',BREAK)
-	os._exit(1)
-	
+        except:
+            break
+
+    client_socket.close()
+    print('Audio closed')
+    os._exit(1)
 
 
 from concurrent.futures import ThreadPoolExecutor
+
 with ThreadPoolExecutor(max_workers=2) as executor:
-	executor.submit(audio_stream)
-	executor.submit(video_stream)
+    executor.submit(audio_stream)
+    executor.submit(video_stream)
